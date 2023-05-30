ea91d38921e23026461ac177627108693b88c9ab
alevy-arcadis <93266090+alevy-arcadis@users.noreply.github.com>
2023-05-30 11:42:38 -0400

    Add project files.

---
+++ TestConverter/Ifc2GlbConverterBase.cs
@@ -0,0 +1,50 @@
+﻿using glTFLoader;
+using glTFLoader.Schema;
+using Xbim.Common.Geometry;
+using Xbim.Common.Step21;
+using Xbim.GLTF;
+using Xbim.Ifc;
+using Xbim.ModelGeometry.Scene;
+
+namespace conversionTest
+{
+    internal static class Ifc2GlbConverterBase
+    {
+        public static byte[] ConvertToGlb(byte[] ifcFile, double deflectionAngle = 100, double deflectionTolerance = 2, XbimSchemaVersion ifcVersion = XbimSchemaVersion.Ifc2X3)
+        {
+            using MemoryStream ms = new(ifcFile);
+            return ConvertToGlb(ms, deflectionAngle, deflectionTolerance, ifcVersion);
+        }
+        public static byte[] ConvertToGlb(Stream ifcFile, double deflectionAngle = 100, double deflectionTolerance = 2, XbimSchemaVersion ifcVersion = XbimSchemaVersion.Ifc2X3)
+        {
+            byte[] glbFileBytes;
+            Gltf gltf;
+            Xbim3DModelContext context;
+            Builder bldr;
+            using (var model = IfcStore.Open(ifcFile, Xbim.IO.StorageType.Ifc, ifcVersion, Xbim.IO.XbimModelType.MemoryModel))
+            {
+
+                model.ModelFactors.ProfileDefLevelOfDetail = 0;
+                model.ModelFactors.DeflectionAngle = deflectionAngle;
+                model.ModelFactors.SimplifyFaceCountThreshHold = 50;
+                model.ModelFactors.DeflectionTolerance = deflectionTolerance;
+                model.ModelFactors.ShortestEdgeLength = 50;
+
+                context = new Xbim3DModelContext(model);
+                context.CreateContext();
+                bldr = new Builder();
+                bldr.BufferInBase64 = true;
+                gltf = bldr.BuildInstancedScene(model, XbimMatrix3D.Identity);
+            }
+
+            using (MemoryStream ms2 = new MemoryStream())
+            {
+                gltf.SaveBinaryModel(null, ms2);
+                glbFileBytes = ms2.ToArray();
+            }
+
+            return glbFileBytes;
+
+        }
+    }
+}
---
+++ TestConverter/Program.cs
@@ -0,0 +1,34 @@
+﻿using conversionTest;
+using System.IO;
+using System.Linq;
+
+// Asset location
+string assetFolder = @"C:\Drive\test\assets\";
+string convertedFolder = @"C:\Drive\test\assets\conv\";
+
+string[] ifcFiles = Directory.GetFiles(assetFolder, "*.ifc");
+
+foreach (string ifcFile in ifcFiles)
+{
+    
+    string glbfileName = ifcFile[(ifcFile.LastIndexOf('\\') + 1)..ifcFile.LastIndexOf('.')];
+
+
+    for (int i = 1; i < 7; i++)
+    {
+        //for (int j = 1; j < 20; j++)
+        {
+            double deflectionAngle = 100;
+            double deflectionTolerance = i * 0.5;
+            string saveFileName = $"{convertedFolder}{glbfileName}-da{deflectionAngle}-dt{deflectionTolerance}.glb";
+            using (Stream fileStream = new FileStream(ifcFile, FileMode.Open, FileAccess.Read))
+            {
+                byte[] convertedFile = Ifc2GlbConverterBase.ConvertToGlb(fileStream, deflectionAngle, deflectionTolerance);
+                using var convertedFileStream = new FileStream(saveFileName, FileMode.Create);
+                convertedFileStream?.Write(convertedFile);
+            }
+            Console.WriteLine(saveFileName + " saved");
+        }
+    }    
+}
+
